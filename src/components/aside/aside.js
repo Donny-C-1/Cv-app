c980f122065d7f3fe493ae70a44dc887b513c8dc
Donny C <99075588+Donny-C-1@users.noreply.github.com>
2023-01-13 19:26:41 +0100

    Completed the desgin of a completed cv

--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -1,5 +1,14 @@
 import './aside.css';
+import cvData from '../../cv.json';
 import avatarImage from '../../images/avatar_hat.jpg';
+import briefcase from '../../images/briefcase-variant.svg';
+import home from '../../images/home.svg';
+import mail from '../../images/email.svg';
+import phone from '../../images/phone.svg';
+import skill from '../../images/asterisk.svg';
+import earth from '../../images/earth.svg';
+
+const { skills, languages } = cvData;
 
 function Avatar(props) {
     return (
@@ -14,24 +23,80 @@ function Info(props) {
     return (
         <div className='info'>
             <ul>
-                <li>Designer</li>
-                <li>London, UK</li>
-                <li>example@mail.com</li>
-                <li>+1 355 443 98</li>
+                <li>
+                    <img src={briefcase} alt='title' />
+                    <span>Designer</span></li>
+                <li>
+                    <img src={home} alt='location' />
+                    <span>London, UK</span>
+                </li>
+                <li>
+                    <img src={mail} alt='email address' />
+                    <span>example@mail.com</span>
+                </li>
+                <li>
+                    <img src={phone} alt='Phone No' />
+                    <span>+1 355 443 98</span>
+                </li>
             </ul>
+        </div>
+    )
+}
+
+function Skills(props) {
+    return (
+        <div>
+            <h2>
+                <img src={skill} alt='skill' />
+                <span>Skills</span>
+            </h2>
+            {skills.map((skill, index) => (
+                <div className='rating' key={index}>
+                    <p>{skill.name}</p>
+                    <div className='level'>
+                        <div style={{ width: skill.level }}>{skill.level}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+function Languages(props) {
+    return (
+        <div>
+            <h2>
+                <img src={earth} alt='languages' />
+                <span>Languages</span>
+            </h2>
+            {languages.map((language, index) => (
+                <div className='rating' key={index}>
+                    <p>{language.name}</p>
+                    <div className='level'>
+                        <div style={{ width: language.level }}>{language.degree}</div>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
 
 function Aside(props) {
     return (
-        <aside className="cont">
-            <Avatar />
-            <div className='inner'>
-                <Info />
-            </div>
-            <p>I'm aside</p>
-        </aside>
+        <div>
+            <aside className="cont">
+                <Avatar />
+                <div className='inner'>
+                    <Info />
+                    <hr />
+                    <Skills />
+                    <br />
+                    <br />
+                    <Languages />
+                    <br />
+                </div>
+            </aside>
+        </div>
     )
 }
 
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,7 +1,52 @@
+import './main.css';
+import cvData from '../../cv.json';
+import calendar from '../../images/calendar-month.svg';
+import briefcase from '../../images/briefcase1.svg';
+import certificate from '../../images/certificate.svg';
+
+const { "work-experience": exp, "education": ed } = cvData
+
 function Main() {
     return (
-        <main className="cont">
-            <p>I'm main</p>
+        <main>
+            <section className="cont">
+                <h2>
+                    <img src={briefcase} alt='work experience' />
+                    <span>Work Experience</span>
+                </h2>
+                {exp.map((experience, index) => (
+                    <article key={index}>
+                        <h3 className='job-title'>{experience['job-title']} / {experience.location}</h3>
+                        <div className='time-period'>
+                            <img src={calendar} alt='calendar' />
+                            <span className='date'>{experience.start} -</span>
+                            {experience.end === null ? <span className='status'>Current</span> : <span>{experience.end}</span>}
+                        </div>
+                        <p>Lorem ipsum sit amet di tolum gea, git na ageh du fileom brught na ebig de sit. Il ve ju anem le du'it ve boujei ge thet.</p>
+                        {index !== exp.length -1 ? <hr /> : <br />}
+                    </article>
+                ))
+                }
+            </section>
+            <br />
+            <section className='cont'>
+                <h2>
+                    <img src={certificate} alt='education' />
+                    <span>Education</span>
+                </h2>
+                {ed.map((education, index) => (
+                    <article key={index}>
+                        <h3>{education.institution}</h3>
+                        <div className='time-period'>
+                            <img src={calendar} alt='calendar' />
+                            <span className='date'>{education.start} </span>
+                            <span>{education.end === null ? "" : "  - " + education.end}</span>
+                        </div>
+                        <p>{education.degree}</p>
+                        {index !== ed.length -1 ? <hr /> : <br />}
+                    </article>
+                ))}
+            </section>
         </main>
     )
 }
